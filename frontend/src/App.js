551a2d650d12f8fc67b066fcdf67979c08564c85
asif <islamasif570@gmail.com>
2021-08-11 13:26:25 +0600

    Footer Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './component/header/header'
+import Footer from './component/footer/footer'
+import Copy from './component/footer/copyright'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
+      <Copy />
     </div>
   );
 }
